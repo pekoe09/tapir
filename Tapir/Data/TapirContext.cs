c123c56edf1c1a6a621e77412e0def3c84baa621
pekoe09 <juha.kangas@welho.com>
2018-09-08 22:52:46 +0300

    Adding a context and basic db connection

---
+++ Tapir/Data/TapirContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Tapir.Models
+{
+    public class TapirContext : DbContext
+    {
+        public TapirContext(DbContextOptions<TapirContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Person> Person { get; set; }
+        public DbSet<Company> Company { get; set; }
+        public DbSet<Employment> Employment { get; set; }
+    }
+}
--- Tapir/Program.cs
+++ Tapir/Program.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Tapir.Models;
 
 namespace Tapir
 {
@@ -14,7 +14,24 @@ namespace Tapir
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<TapirContext>();
+                    context.Database.EnsureCreated();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred when creating the database");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
