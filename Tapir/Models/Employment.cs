d3b828ed3c4e7fde5f18ab7d3ef2d2b9372c791c
pekoe09 <juha.kangas@welho.com>
2018-09-08 20:53:08 +0300

    First models

---
+++ Tapir/Models/Employment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tapir.Models
+{
+    public class Employment
+    {
+        public int ID { get; set; }
+        [Required]
+        public int CompanyID { get; set; }
+        [Required]
+        public int PersonID { get; set; }
+
+        public Company Company { get; set; }
+        public Person Person { get; set; }
+    }
+}
