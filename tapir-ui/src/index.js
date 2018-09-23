eaae65491b7b7170667f6db1bb09c277cb7b8739
pekoe09 <juha.kangas@welho.com>
2018-09-23 16:04:19 +0300

    Added UI project with basic React scaffold

---
+++ tapir-ui/src/index.js
@@ -0,0 +1,23 @@
+﻿import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from './store'
+
+const render = () => {
+    ReactDOM.render(
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <Router>
+                    <App />
+                </Router>
+            </PersistGate>
+        </Provider>,
+        document.getElementById('root')
+    )
+}
+
+render()
+store.subscribe(render)
